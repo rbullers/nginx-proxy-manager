0b96029df6a6cc5e53b6c4fe85e93a81d5d07c29
rbullers <88460527+rbullers@users.noreply.github.com>
2023-05-07 14:07:28 +0100

    Create nginx-dynamic.sh

---
+++ docker/scripts/nginx-dynamic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash env
+# Autowhitelist multiple DDNS Addresses for nginx
+# Author: Mike from https://guides.wp-bullet.com/
+
+#define Dynamic DNS addresses here
+DDNS[0]="rbullers.duckdns.org"
+
+#create an array of the dynamic IPs
+if [ ! -f /etc/nginx/conf.d/dynamicips ]; then
+        for DNS in "${DDNS[@]}"
+        do
+            echo "allow $(dig x +short $DNS);" >> /etc/nginx/conf.d/dynamicips
+        done
+fi
+
+#create current array for comparison with fresh IPs
+CURRENT=($(cat /etc/nginx/conf.d/dynamicips))
+#Generate fresh list
+let "DNSCOUNT=${#DDNS[@]} - 1"
+for i in $(eval echo "{0..$DNSCOUNT}")
+	do
+	    FRESH[$i]="allow $(dig x +short ${DDNS[$i]});"
+	done
+
+#compare old list and new list, create new file if there are differences
+CURRENTAGG=${CURRENT[@]};
+FRESHAGG=${FRESH[@]};
+if [ "$CURRENTAGG" != "$FRESHAGG" ]; then
+	rm /etc/nginx/conf.d/dynamicips
+	for i in $(eval echo "{0..$DNSCOUNT}")
+	    do
+                echo "${FRESH[$i]}" >> /etc/nginx/conf.d/dynamicips
+	    done
+	service nginx reload
+fi
